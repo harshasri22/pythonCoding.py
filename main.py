2b6c67674ee7104696372df0148d5843247f57c5
Harsha Srivastava <hs6414008@gmail.com>
2023-02-21 15:11:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+num1=10
+num2=20
+print("Value of num1 before swapping:",num1)
+print("Value of num2 before swapping:",num2)
+
+temp=num1  # 10
+num1=num2  # 20
+num2=temp  # 10
+
+print("Value of num1 after swapping:",num1)
+print("Value of num2 after swapping",num2)
